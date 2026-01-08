743b9784d38f3c7523eca0d47858225b4b3e68f1
QuadTrack <236016848+QuadTrack@users.noreply.github.com>
2026-01-08 13:17:58 +0300

    created mingw

---
+++ win_build/CMakeFiles/4.2.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.18.2-zen2-1-zen")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.18.2-zen2-1-zen")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/usec/Git/kusaiGUI/mingw.cmake")
+
+set(CMAKE_SYSTEM "Windows")
+set(CMAKE_SYSTEM_NAME "Windows")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
